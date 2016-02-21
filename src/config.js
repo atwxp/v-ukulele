f543dd54ec7770328dec8ba3b7219ec26550862e
wupanpan <wupanpan@baidu.com>
2016-02-22 02:39:14 +0800

    assets path

--- src/config.js
+++ src/config.js
@@ -5,67 +5,66 @@ module.exports = {
             id: '1',
             author: '武小盼',
             title: '口琴版 - 那些花儿',
-            url: '/assets/audio/口琴版那些花儿',
-            cover: '/assets/audio/cover/tortoise.jpg'
+            url: '/v-ukulele/assets/audio/口琴版那些花儿',
+            cover: '/v-ukulele/assets/audio/cover/tortoise.jpg'
         },
 
         '2': {
             id: '2',
             author: 'wxp',
             title: '口琴版 - 暖暖',
-            url: '/assets/audio/口琴版暖暖',
-            cover: '/assets/audio/cover/tortoise.jpg'
+            url: '/v-ukulele/assets/audio/口琴版暖暖',
+            cover: '/v-ukulele/assets/audio/cover/tortoise.jpg'
         },
 
         '3': {
             id: '3',
             author: 'wxp',
             title: 'ukulele - 小手拉大手',
-            url: '/assets/audio/小手拉大手',
-            cover: '/assets/audio/cover/tortoise.jpg'
+            url: '/v-ukulele/assets/audio/小手拉大手',
+            cover: '/v-ukulele/assets/audio/cover/tortoise.jpg'
         },
 
         '4': {
             id: '4',
             author: 'wxp',
             title: 'ukulele - 宝贝',
-            url: '/assets/audio/宝贝前奏',
-            cover: '/assets/audio/cover/tortoise.jpg'
+            url: '/v-ukulele/assets/audio/宝贝前奏',
+            cover: '/v-ukulele/assets/audio/cover/tortoise.jpg'
         },
 
         '5': {
             id: '5',
             author: 'wxp',
             title: 'ukulele - 对视',
-            url: '/assets/audio/对视',
-            cover: '/assets/audio/cover/tortoise.jpg'
+            url: '/v-ukulele/assets/audio/对视',
+            cover: '/v-ukulele/assets/audio/cover/tortoise.jpg'
         },
 
         '6': {
             id: '6',
             author: 'wxp',
             title: 'ukulele - i m yours 前奏',
-            url: '/assets/audio/i m yours前奏',
-            cover: '/assets/audio/cover/tortoise.jpg'
+            url: '/v-ukulele/assets/audio/i m yours前奏',
+            cover: '/v-ukulele/assets/audio/cover/tortoise.jpg'
         },
 
         '7': {
             id: '7',
             author: 'wxp',
             title: 'ukulele - 暖暖前奏',
-            url: '/assets/audio/暖暖前奏',
-            cover: '/assets/audio/cover/tortoise.jpg'
+            url: '/v-ukulele/assets/audio/暖暖前奏',
+            cover: '/v-ukulele/assets/audio/cover/tortoise.jpg'
         },
 
         '8': {
             id: '8',
             author: 'wxp',
             title: 'ukulele - 陪我看日出前奏',
-            url: '/assets/audio/陪我看日出前奏',
-            cover: '/assets/audio/cover/tortoise.jpg'
+            url: '/v-ukulele/assets/audio/陪我看日出前奏',
+            cover: '/v-ukulele/assets/audio/cover/tortoise.jpg'
         }
     },
 
-    albums: ['1', '2', '3', '4', '5']
-
-}
+    albums: ['1', '2', '3', '4', '5', '6', '7', '8']
+};
